346dfb52bd7536463e3911109b1c97143c17a194
gmoermans <guillaume.moermans@gmail.com>
2020-11-30 09:40:44 +0100

    new background system

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
-$(document).ready(function(){
-  $('.header').height($(window).height());
-})
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "yourusername",
+  password: "yourpassword"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+  con.query("CREATE DATABASE mydb", function (err, result) {
+    if (err) throw err;
+    console.log("Database created");
+  });
+});
