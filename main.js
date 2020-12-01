c149daa364deff8860e14c044d17a076c9d530be
gmoermans <guillaume.moermans@gmail.com>
2020-12-01 22:42:47 +0100

    script splited

--- main.js
+++ main.js
@@ -12,9 +12,9 @@ for (var i = 0; i < images.length; i++) {
   var img = images[i];
   // and attach our click listener for this image.
   img.onclick = function(evt) {
-    modal.style.display = "block";
-    modalImg.src = this.src;
-    captionText.innerHTML = this.alt;
+	modal.style.display = "block";
+	modalImg.src = this.src;
+	captionText.innerHTML = this.alt;
   }
 }
 
