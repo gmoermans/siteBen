77130885f69ae384bb1c7f2da9d4fe711e1c2c67
gmoermans <guillaume.moermans@gmail.com>
2020-12-02 13:28:16 +0100

    add formular

--- sender.php
+++ sender.php
@@ -1,61 +1,61 @@
 <?php
+if(!isset($_POST['submit']))
+{
+	//This page should not be accessed directly. Need to submit the form.
+	echo "error; you need to submit the form!";
+}
+$name = $_POST['user_name'];
+$visitor_email = $_POST['user_email'];
+$message = $_POST['user_message'];
 
+//Validate first
+if(empty($name)||empty($visitor_email)) 
+{
+    echo "Name and email are mandatory!";
+    exit;
+}
+
+if(IsInjected($visitor_email))
+{
+    echo "Bad email value!";
+    exit;
+}
+
+$email_from = $visitor_email;//<== update the email address
+$email_subject = "Message de formulaire";
+$email_body = "Message de $name.\n".
+    ":\n $message".
+    
+$to = "r6elsa@outlook.com";//<== update the email address
+$headers = "From: $email_from \r\n";
+$headers .= "Reply-To: $visitor_email \r\n";
+//Send the email!
+mail($to,$email_subject,$email_body,$headers);
+//done. redirect to thank-you page.
+header('index.html');
+
+
+// Function to validate against any email injection attempts
 function IsInjected($str)
 {
-	$injections = array('(\n+)',
-		   '(\r+)',
-		   '(\t+)',
-		   '(%0A+)',
-		   '(%0D+)',
-		   '(%08+)',
-		   '(%09+)'
-		   );
-			   
-	$inject = join('|', $injections);
-	$inject = "/$inject/i";
-	
-	if(preg_match($inject,$str))
-	{
-	  return true;
-	}
-	else
-	{
-	  return false;
-	}
+  $injections = array('(\n+)',
+              '(\r+)',
+              '(\t+)',
+              '(%0A+)',
+              '(%0D+)',
+              '(%08+)',
+              '(%09+)'
+              );
+  $inject = join('|', $injections);
+  $inject = "/$inject/i";
+  if(preg_match($inject,$str))
+    {
+    return true;
+  }
+  else
+    {
+    return false;
+  }
 }
-
-function sendEmail()
-{
-	echo "send email called!";	
-	$name = $_POST['user_name'];
-	$visitor_email = $_POST['user_mail'];
-
-	if(IsInjected($visitor_email))
-	{
-		echo "Bad email value!";
-		exit;
-	}
-
-	echo "injection attack safe";
-
-	$message = $_POST['user_message'];
-	  
-	$email_from = 'r6elsa@outlook.com';
-	$email_subject = "requete via formulaire de contact";
-	$email_body = "message de $name.\n".
-	"message :\n $message".
-	"adresse mail destinataire : $visitor_email".
-
-
-	if(mail($email_from,$email_subject,$email_body,""))
-	{
-		echo "Mail send!";
-	}
-	else
-	{
-		echo "mail not send";
-	}	
-}
-
-	
-?>
+   
+?> 
