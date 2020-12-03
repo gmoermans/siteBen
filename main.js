9ab76992e7d9f9064481076ea8860a8fdfc7df32
gmoermans <guillaume.moermans@gmail.com>
2020-12-03 16:31:40 +0100

    new version

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+console.log("main called ");
 // create references to the modal...
 var modal = document.getElementById('myModal');
 // to all images -- note I'm using a class!
@@ -7,6 +8,45 @@ var modalImg = document.getElementById("img01");
 // and the caption in the modal
 var captionText = document.getElementById("caption");
 
+
+
+
+/*Test for scroll*/
+function isScrolledIntoView(elem)
+{
+    var docViewTop = $(window).scrollTop();
+    var docViewBottom = docViewTop + $(window).height();
+
+    var elemTop = $(elem).offset().top;
+    var elemBottom = elemTop + $(elem).height();
+
+    return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+}
+
+// when you scroll the div
+$(".list").scroll(function(e) {
+    var $list = $(this);
+
+    // apply logic only to visible elements
+    $list.find("div")
+        .filter(function(i, d) {
+            return isScrolledIntoView(d);
+        })
+        .each(function() {
+            var eTop = $(this).offset().top;
+            var center = $list.height() / 2;
+
+            // if the element is in the center it is 100%
+            // otherwise it will fade
+            var dif = center - eTop;
+            if (dif < 0) dif *= -1;
+
+            var pc = 1 - (dif / center);
+
+            // set the opacity for the elements
+            $(this).css("opacity", pc);
+        });
+});
 // Go through all of the images with our custom class
 for (var i = 0; i < images.length; i++) {
   var img = images[i];
---
+++ main2.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+	console.log("main2 called");
+  //Take your div into one js variable
+  var div = $("#divContact");
+  //Take the current position (vertical position from top) of your div in the variable
+  var pos = div.position();
+  //Now when scroll event trigger do following
+  $(window).scroll(function () {
+   var windowpos = $(window).scrollTop();
+   //Now if you scroll more than 100 pixels vertically change the class to AfterScroll
+   // I am taking 100px scroll, you can take whatever you need
+   if (windowpos >= (pos.top - 10)) {
+	   console.log("i display the calss ");
+     div.addClass("afterScroll");
+   }
+   //If scroll is less than 100px, remove the class AfterScroll so that your content will be hidden again 
+   else {
+     s.removeClass("afterScroll");
+	 console.log("i hide the calss ");
+   }
+   //Note: If you want the content should be shown always once you scroll and do not want to hide it again when go to top agian, no need to write the else part
+ });
+});
